e0c025e50084c92301c60662e2d2679b5108d78d
Gitenax <duginlive@gmail.com>
2021-02-09 22:15:46 +0300

    Работа с сохранениями разделена на два отдельных класса

---
+++ Assets/Scripts/Data/GameDataReader.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class GameDataReader
+{
+    private string _fileName;
+    
+    public GameDataReader(string fileName) => _fileName = fileName;
+    
+    public GameData Read()
+    {
+        string path = Application.persistentDataPath + $"/{_fileName}.dat";
+        if (File.Exists(path))
+        {
+            using (FileStream stream = File.OpenRead(path))
+            {
+                return new BinaryFormatter().Deserialize(stream) as GameData;
+#if UNITY_EDITOR
+                Debug.Log("<color=green>Файл сохранения загружен!</color>");
+#endif
+            }
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning("<color=red>Файл сохранения отсутствует!</color>");
+#endif
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/Data/GameDataWriter.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class GameDataWriter
+{
+    private string _fileName;
+    
+    public GameDataWriter(string fileName) => _fileName = fileName;
+    
+    public void Write(GameData data)
+    {
+        using (FileStream stream = File.Create(Application.persistentDataPath + $"/{_fileName}.dat"))
+        {
+            new BinaryFormatter().Serialize(stream, data);
+#if UNITY_EDITOR
+            Debug.Log("<color=green>Данные сохранены!</color>");
+#endif
+        }
+    }
+}
