624ca50cef3d1943f4c604a5a9ebf61230521ee5
Gitenax <duginlive@gmail.com>
2021-02-09 22:16:37 +0300

    Добавлен класс для смены цвета текста на переключателе в зависимости от положения <Toggle>

---
+++ Assets/Scripts/Controls/ToggleLabel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle)), ExecuteAlways]
+public class ToggleLabel : MonoBehaviour
+{
+    [SerializeField] private Text _label;
+    [SerializeField] private Toggle _toggle;
+    
+    private void OnEnable()
+    {
+        _toggle = GetComponent<Toggle>();
+        _toggle.onValueChanged.AddListener(OnChangedValue);
+    }
+
+    private void OnDisable()
+    {
+        _toggle.onValueChanged.RemoveListener(OnChangedValue);
+    }
+
+    private void OnChangedValue(bool state)
+    {
+        if(state)
+            _label.color = Color.black;
+        else
+            _label.color = Color.white;
+    }
+}
